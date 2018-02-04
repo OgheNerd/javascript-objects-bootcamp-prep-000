55eab197ae31237a89fba1a049842d8c9edddf56
Oghene Michael <zonalmeet@gmail.com>
2018-02-04 08:34:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist = {artistName: 'song'};
+var playlist = {artist: 'song'};
 
-updatePlaylist(playlist, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
