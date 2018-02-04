fdd606258a97598787454bbaadb69aa4cdc198ab
Oghene Michael <zonalmeet@gmail.com>
2018-02-04 08:48:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   return delete playlist['artistName'];
-         playlist
 }
+
+removeFromPlaylist(playlist);
